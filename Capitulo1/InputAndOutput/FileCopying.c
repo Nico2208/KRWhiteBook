552a8a0fdec00209c7dd3e07b14219b67f112390
Nicolas Sicolo <sicolo2208@gmail.com>
2023-09-22 00:18:40 -0300

    Agrego File Copying

--- Capitulo1/InputAndOutput/FileCopying.c
+++ Capitulo1/InputAndOutput/FileCopying.c
@@ -1,2 +1,13 @@
 //Programa que copia el input en output un caracter a la vez.
 
+#include <stdio.h>
+
+int main() {
+    int c;
+    c = getchar(); //lee el siguiente caracter ingresado en el text stream.
+
+    while( c != EOF ) {
+        putchar(c); //Imprime un caracter cada vez que es llamado. 
+        c = getchar();
+    }
+}
